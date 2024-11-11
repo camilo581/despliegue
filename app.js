ba90afbcd252b9cf73f44b397379ef310814f3f8
camilo581 <camilodelgado59@gmail.com>
2024-11-11 14:31:35 -0500

    cambio

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ const cors = require("cors");
 const dbconnect = require("./config");
 const app = express();
 
-
-
-
-
 app.use(express.json());
-app.use(cors({origin:"http://localhost:4200"}));
+app.use(cors({origin:"*"}));
 app.use("/api/usuarios" , require("./routes/usuario.routes"));
 app.use("/api/empleados" , require("./routes/empleado.routes"));
 
