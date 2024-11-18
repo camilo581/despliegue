377bdfeb33c3cd6774d4774e27b269cc3b1163c3
camilo581 <camilodelgado59@gmail.com>
2024-11-18 12:06:22 -0500

    actulizacion

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require ('dotenv').config()
+require ('dotenv').config();
 const dbconnect =() => {
     mongoose.set("strictQuery" , true)
     mongoose.connect(process.env.MONGODB_URL)
