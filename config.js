e30514f412769be4576053de4e9f6c93db6fd1d3
camilo581 <camilodelgado59@gmail.com>
2024-11-18 13:48:25 -0500

    actualizar

--- config.js
+++ config.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
-require ('dotenv').config();
-const dbconnect =() => {
-    mongoose.set("strictQuery" , true)
-    mongoose.connect(process.env.MONGODB_URL)
-    .then((success)=> console.log("conexion exitosa"))
-    .catch((err)=> console.log(err.message));
-}
+require("dotenv").config();
+
+const dbconnect = async () => {
+    try {
+        if (!process.env.MONGODB_URL) {
+            console.error("Error: MONGODB_URL no está definida en el archivo .env");
+            process.exit(1);
+        }
+
+        mongoose.set("strictQuery", true);
+
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("Conexión exitosa a MongoDB");
+    } catch (err) {
+        console.error("Error al conectar a MongoDB:", err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = dbconnect;
 
 
-module.exports =dbconnect;
 
 
 
